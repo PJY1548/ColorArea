3af3f0d29bd1a8f533e478e035613ce232b8ce14
PJY1548 <104071676+PJY1548@users.noreply.github.com>
2026-01-18 11:15:30 +0800

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-npm install -g wrangler
-npx wrangler@latest login
-echo 上传 KV
-for %%F in (*.html) do (
-	echo 上传 %%F ...
-	npx wrangler@latest kv key put "%%~nxF" --path "%%~fF" --binding PUBLIC_ASSETS --preview false --remote
-)
-wrangler deploy
